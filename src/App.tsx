a64d56e1e247bafa1dbc3b7def2f8b07aba69a2c
florian-deroo <florian.deroo59@gmail.com>
2023-02-15 14:56:50 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
-import { Metaplex } from '@metaplex-foundation/js';
+import './App.css'
 import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
-import { useEffect, useMemo, useState } from 'react';
+import { Metaplex } from '@metaplex-foundation/js';
+
+const connection = new Connection(clusterApiUrl("devnet"));
+const mx = Metaplex.make(connection);
 
 export default function App() {
-    const [nfts, setNfts] = useState<any[]>([]);
 
-    const connection = new Connection(clusterApiUrl('devnet'));
+  mx.nfts().findAllByOwner({ owner: new PublicKey("E7mVt4rjuqFLWajKHStebUyzkQ9Z9NBQKpP2t3V9aEZ5") }).then(r => console.log(r))
 
-    const mx = useMemo(() => {
-        return Metaplex.make(connection);
-    }, [connection]);
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const results = await mx
-                    .nfts()
-                    .findAllByOwner({
-                        owner: new PublicKey(
-                            'E7mVt4rjuqFLWajKHStebUyzkQ9Z9NBQKpP2t3V9aEZ5'
-                        ),
-                    });
-                setNfts(results);
-            } catch (error) {
-                console.log(error);
-            }
-        })();
-    }, [mx]);
-
-    useEffect(() => {
-        console.log(nfts);
-    }, [nfts]);
-
-    return <div></div>;
+  return (
+      <div>
+        
+      </div>
+  )
 }
