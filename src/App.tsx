503288bdb98359351c648e90f393dfdd211a6101
florian-deroo <florian.deroo59@gmail.com>
2023-02-15 01:28:54 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import './App.css'
 import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
 import { Metaplex } from '@metaplex-foundation/js';
-import { useWallet } from '@solana/wallet-adapter-react';
-
-
 
 async function fetchNft(publicKey: PublicKey | null, mx: Metaplex) {
   if (!publicKey) return null;
@@ -17,9 +14,8 @@ const connection = new Connection(clusterApiUrl("devnet"));
 const mx = Metaplex.make(connection);
 
 export default function App() {
-  const { publicKey, sendTransaction } = useWallet();
 
-  fetchNft(publicKey, mx)
+  fetchNft(new PublicKey("E7mVt4rjuqFLWajKHStebUyzkQ9Z9NBQKpP2t3V9aEZ5"), mx)
 
   return (
       <div>
